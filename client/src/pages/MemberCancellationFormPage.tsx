2096aacc7f8bbe44a5b6aca8e554342ca3fe122b
Filipp-Pogo <fpogostkin@woodinvillesportsclub.com>
2026-06-04 20:42:10 -0700

    Fix membership request hero crop

--- client/src/pages/MemberCancellationFormPage.tsx
+++ client/src/pages/MemberCancellationFormPage.tsx
@@ -17,6 +17,8 @@ export default function MemberCancellationFormPage() {
         headline="Membership Cancellation Requests."
         subtitle="Send your request to the WSC team so we can review your account and follow up."
         image={HERO_IMG}
+        imagePosition="center top"
+        avoidHeaderCrop
       />
       <section className="bg-parchment px-6 lg:px-14 py-20 lg:py-24">
         <div className="max-w-[860px] mx-auto">
